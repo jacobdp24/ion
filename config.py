0b4661fe8dffe90f6ea20a1f4367211f2bac63c0
Jacob Doering-Powell <jacobdoeringpowell@gmail.com>
2018-10-08 23:48:40 -0700

    changed message to html

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
-subject = 'origins of the newsletter'
+subject = 'links initiated'
 
 message = '''
-this is a test and I hope it works
+<p>this is a test and I hope it works, and join our facebook page <a href=\"https://www.facebook.com/groups/988219401317312/?multi_permalinks=1200504633422120&notif_id=1539065071444945&notif_t=group_activity\">here</a></p>
 '''
